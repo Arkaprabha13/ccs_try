1cbdbc9054fd603c0db9271dabf28b8db4b26bfd
Arkaprabha Banerjee <arkaofficial13@gmail.com>
2025-03-16 20:13:56 +0530

    2nd

---
+++ qr_generator.py
@@ -0,0 +1,20 @@
+import qrcode
+
+# URL of the hosted webpage
+url = "https://arkaprabha13.github.io/ccs_try/"
+
+# Generate QR Code
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(url)
+qr.make(fit=True)
+
+# Create an image from the QR Code instance
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the QR Code image
+img.save("consituency16.png")
